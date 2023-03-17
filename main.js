86239e67c7d4219731e8b089cd6ea25cbb9e46c4
AWarno <anulka.warno@gmail.com>
2023-03-17 03:50:07 +0100

    counter and spotify playlist added

--- main.js
+++ main.js
@@ -8,3 +8,34 @@ mediaButton.onclick = function () {
     mainListDiv.classList.toggle("show_list");
     mediaButton.classList.toggle("active");
 };
+
+function countdownTimer(dateString) {
+    // Set the date we're counting down to
+    var countDownDate = new Date(dateString).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Calculate days and hours
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+      // Output the result in an element with id="countdown"
+      document.getElementById("countdown").innerHTML = "Days: " + days + " and Hours: " + hours;
+
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+      }
+    }, 9000);
+  }
+
+  // Call the countdownTimer function with the date string
+  countdownTimer("June 17, 2023 00:00:00");
